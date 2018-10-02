149be35e9ce849d5aefb328d349090ab4e5d7cb7
Nandhini <nandhinismart55@gmail.com>
2018-10-02 14:30:44 +0530

    js file update

--- js/index.js
+++ js/index.js
@@ -57,7 +57,7 @@ $(document).ready(function() {
         profile_details	+=	'</div>';
         profile_details	+=	'<div class="buttons-div">';
        // profile_details	+=	'<a href="http://www.synthridersvr.com/" target="_blank" class="button-bottom no-right-border w-button">Site</a>';
-        profile_details	+=	'<a href="https://twitter.com/'+handle_name+'" target="_blank" class="button-bottom right-button w-button">Twitter</a>';
+        profile_details	+=	'<a href="'+handle_name+'" target="_blank" class="button-bottom right-button w-button">Twitter</a>';
         profile_details	+=	'</div>';
 		profile_details	+=	'</div>';
 
